d0983b16785969d79b46e534c4b9f07ca1e4ecfa
JUDA14 <rictus14@hotmail.fr>
2019-05-07 10:22:37 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div>
+            <h1>CLASSE A4</h1>
+            <h3>DATE : 10/05/2019</h3>
+        </div>
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Heure arrivée</th>
+                    <th>Heure partie</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="entry in entries" :key="entry.id">
+                        <td>{{entry.nom}}</td>
+                        <td>{{entry.prenom}}</td>
+                        <td>{{entry.heure_arrive}}</td>
+                        <td>{{entry.heure_depart}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data () {
+            return {
+                entries: [
+                    {
+                        id: 1,
+                        nom: 'Lemoigne',
+                        prenom: 'Clément',
+                        heure_arrive: '8H45',
+                        heure_depart: "17H01"
+                    },
+                    {
+                        id: 2,
+                        nom: 'Guillemette',
+                        prenom: 'Florent',
+                        heure_arrive: '9H10',
+                        heure_depart: "17H04"
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
